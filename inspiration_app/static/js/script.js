d2bd841669ff320c8b4fb844b8c1e03c60915e57
Said <saidziani.pro@gmail.com>
2019-02-24 11:41:59 -0700

    Merging front-end, replacing some images and adding JS

--- inspiration_app/static/js/script.js
+++ inspiration_app/static/js/script.js
@@ -1,15 +1,6 @@
-const link = document.querySelector('#formLink').innerHTML
-const embedElement = document.querySelector('#form')
-typeformEmbed.makeWidget(
-    embedElement,
-    link, // NOTE: Replace with your typeform URL
-    {
-    hideHeaders: true,
-    hideFooter: true,
-    opacity: 0,
-    buttonText: "Find your taste in music!",
-    onSubmit: function () {
-        console.log('Preferences successfully submitted')
-    }
-    }
-)
+$(document).ready(function(){
+
+
+});
+
+
